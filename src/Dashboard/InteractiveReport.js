8f605cef08dad9a558df1f9fcf1c0ff1fa037f19
trandangbaonhi3008 <tr.baonhi3008@gmail.com>
2022-09-05 21:08:12 +0700

    changes

--- src/Dashboard/InteractiveReport.js
+++ src/Dashboard/InteractiveReport.js
@@ -7,9 +7,9 @@ const IframeCustomized = styled.div`
 `;
 function InteractiveReport (){
 		return <div>
-			<div>Interactive Report</div>
+			<h3>Interactive Report</h3>
 			<IframeCustomized> 
-			<iframe src="https://baonhi3008.github.io/Dart-interactive/" width="1570" height ="1300" />;
+			<iframe src="https://baonhi3008.github.io/Dart-interactive/" width="1200" height ="1300" />;
 			{/* <iframe src="https://baonhi3008.github.io/DART-testing/" width="1570" height ="1300" />; */}
 			</IframeCustomized>
 			
